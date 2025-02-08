143dad49c476f7de747a38db7ce061286d5be15a
Gleb <glebshm@rambler.ru>
2025-02-08 22:26:44 +0300

    feat: complete task

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 class Queue {
-
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  enqueue(value) {
+    let node = new ListNode(value);
+    
+    if (this.tail !== null) {
+     this.tail.next = node;
+    } else {
+      this.head = node;
+    }
 
+    this.tail = node;
+    this.size++
+    
+  }
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.head !== null && this.head.next !== null) {
+      let temp = this.head.value;
+      this.head = this.head.next
+      this.size--
+      return temp
+    }
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
